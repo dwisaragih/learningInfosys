9d93e67bed2556a940f964f739d2769072dccafc
dwi Saragih <dwi.saragih@ist.id>
2021-12-07 09:51:30 +0700

    task1 1

---
+++ src/main/java/com/infosys/learning/controller/LearningController.java
@@ -0,0 +1,20 @@
+package com.infosys.learning.controller;
+
+import com.infosys.learning.service.LearningService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api")
+public class LearningController {
+    @Autowired
+    LearningService learningService;
+
+    @GetMapping(value = "/getpersonname")
+    public String getPersonName(@RequestParam(value = "gender", defaultValue = "gender")String gender){
+        return learningService.getName(gender);
+    }
+}
---
+++ src/main/java/com/infosys/learning/service/LearningService.java
@@ -0,0 +1,20 @@
+package com.infosys.learning.service;
+
+import com.infosys.learning.dto.Person;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LearningService {
+
+    public String getName(String gender){
+        Person person = new Person();
+
+        if ("man".equals(gender)){
+            person.setName("John");
+        }
+        else{
+            person.setName("Jane");
+        }
+        return person.getName();
+    }
+}
